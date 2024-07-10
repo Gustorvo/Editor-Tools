d91f631566f2833170d7cfc023b3bab7fa1159fb
Gustorvo <mumiasms@gmail.com>
2024-07-10 19:18:11 +0200

    add Built-in support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
 ## [1.0.0] - 2024-07-06
 ## First Release
 - Add Editor tools
+## [1.0.1] - 2024-07-10
+## Added Built-in version
+- Add Built-in version
--- Editor/ToolsMenu.cs
+++ Editor/ToolsMenu.cs
@@ -12,10 +12,18 @@ namespace Gustorvo.Tools
             Refresh();
         }
 
-        [MenuItem("Tools/Load Meta Ready Manifest")]
-        static async void LoadMetaReadyManifest()
+        [MenuItem("Tools/Load Meta Ready Manifest (URP)")]
+        static async void LoadMetaReadyManifestURP()
         {
             var url = Packages.GetGistURL("ba8c1ab508075ab5ea7c01069c299e62");
+            var contents = await Packages.GetContent(url);
+            Packages.ReplacePackageFile(contents);
+        }
+        
+        [MenuItem("Tools/Load Meta Ready Manifest (Built-in)")]
+        static async void LoadMetaReadyManifestBuintIn()
+        {
+            var url = Packages.GetGistURL("a3502b5613d3bdff6d993417b6aaa730");
             var contents = await Packages.GetContent(url);
             Packages.ReplacePackageFile(contents);
         }
